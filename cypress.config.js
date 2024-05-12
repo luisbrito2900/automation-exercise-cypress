7ce2cfda280ad018fdd0b1e179fac16fe014dc98
luisbrito2900 <luisjahziel1029@gmail.com>
2024-05-12 02:40:49 -0400

    adding reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,36 @@
 const { defineConfig } = require("cypress");
+const fs = require("fs");
+const path = require("path");
 
 module.exports = defineConfig({
+  video: true,
+  reporter: "mochawesome",
+  reporterOptions: {
+    charts: true,
+    reportDir: "cypress/results",
+    reportFilename: "e2e-tests-report.html",
+    overwrite: true,
+    html: true,
+    json: false,
+  },
   e2e: {
     baseUrl: "https://automationexercise.com/",
     testIsolation: false,
     viewportWidth: 1920,
     viewportHeight: 1080,
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) {
+      on("before:run", async () => {
+        const reportDir = path.join(
+          __dirname,
+          config.reporterOptions.reportDir
+        );
+        console.log(`Clearing report directory: ${reportDir}`);
+
+        if (fs.existsSync(reportDir)) {
+          fs.rmSync(reportDir, { recursive: true, force: true });
+          console.log(`Report directory cleared: ${reportDir}`);
+        }
+      });
+    },
   },
 });
